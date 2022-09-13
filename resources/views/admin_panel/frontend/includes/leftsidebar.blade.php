1955d19f817750e4f0c16dcf2a11ebb12a9bc57f
SUBHAN <lionsubhan123@gmail.com>
2022-09-13 18:05:14 +0500

    Left Side bar

--- resources/views/admin_panel/frontend/includes/leftsidebar.blade.php
+++ resources/views/admin_panel/frontend/includes/leftsidebar.blade.php
@@ -3,7 +3,7 @@
     <div class="app-sidebar__overlay" data-bs-toggle="sidebar"></div>
     <div class="app-sidebar">
         <div class="side-header">
-            <a class="header-brand1" href="index.html">
+            <a class="header-brand1" href="#">
                 <!-- <img src="{{asset('assets/images/brand/toolprime-sidebar-rplace.png')}}" class="header-brand-img light-logo" alt="logo"> -->
                 <!-- <img src="{{asset('assets/images/brand/toolprime-sidebar.png')}}" class="header-brand-img light-logo1" alt="logo"> -->
                 <h3>Dr24Tele</h3>
