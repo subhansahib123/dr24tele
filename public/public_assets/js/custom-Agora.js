ada5187c312926d762dc87ebd35264ce7b56e3ff
hassan ali <hassanali5120@gmail.com>
2022-10-24 14:39:29 +0500

    dddddttttt

--- public/public_assets/js/custom-Agora.js
+++ public/public_assets/js/custom-Agora.js
@@ -3,18 +3,23 @@ $("#video-btn").prop("disabled", true);
 $("#screen-share-btn").prop("disabled", true);
 $("#exit-btn").prop("disabled", true);
 
-$(document).ready(function () {
-    $("#modalForm").modal("show");
-});
-
-$("#join-channel").click(function (event) {
-    var agoraAppId = $("#form-appid").val();
-    var token = $("#form-token").val();
-    var channelName = $("#form-channel").val();
-    var uid = parseInt($("#form-uid").val());
-    $("#modalForm").modal("hide");
-    initClientAndJoinChannel(agoraAppId, token, channelName, uid);
-});
+// $(document).ready(function () {
+//     $("#modalForm").modal("show");
+// });
+var agoraAppId = $("#form-appid").val();
+var token = $("#form-token").val();
+var channelName = $("#form-channel").val();
+var uid = parseInt($("#form-uid").val());
+// $("#modalForm").modal("hide");
+initClientAndJoinChannel(agoraAppId, token, channelName, uid);
+// $("#join-channel").click(function (event) {
+//     var agoraAppId = $("#form-appid").val();
+//     var token = $("#form-token").val();
+//     var channelName = $("#form-channel").val();
+//     var uid = parseInt($("#form-uid").val());
+//     $("#modalForm").modal("hide");
+//     initClientAndJoinChannel(agoraAppId, token, channelName, uid);
+// });
 
 
                 // $.ajax({
