bf50262a198af30e5b8cbdbe69b36cebe0d074f9
SUBHAN <lionsubhan123@gmail.com>
2022-11-21 19:14:45 +0500

    update 010

---
+++ app/Http/Controllers/Admin/SpecializationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\DoctorSpecialization;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SpecializationController extends Controller
+{
+    
+}
