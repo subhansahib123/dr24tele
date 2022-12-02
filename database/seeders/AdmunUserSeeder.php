a47dec49f9d6beacc7af199f34008f4ce48659e0
faridtuf <ghulam.farid@tuf.edu.pk>
2022-12-02 14:48:43 +0500

    Update Seeder

--- database/seeders/AdmunUserSeeder.php
+++ database/seeders/AdmunUserSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User_Role;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Profession;
+use Illuminate\Support\Str;
+
 class AdmunUserSeeder extends Seeder
 {
     /**
@@ -17,22 +21,35 @@ class AdmunUserSeeder extends Seeder
         Profession::create([
             'name'=>'MasterProfession'
         ]);
-        $user = User::create([
-            'username' => 'nissi',
-            'email'=>'nissi@drtele.co',
-            'phone_number'=>'9715555555',
-            'password' => bcrypt('nissi@drtele'),
-            'profession_id'=>1,
-            'status'=>1
-        ]);
-        $user = User::create([
-            'username' => 'akhil',
-            'email'=>'akhil@drtele.co',
-            'phone_number'=>'97155555525',
-            'password' => bcrypt('akhil@drtele'),
-            'profession_id'=>1,
-            'status'=>1
-        ]);
+        $roles =[
+            ['EHRNAdmin','ehrnadmin'],
+            ['FrontOffice','frontoffice'],
+            ['OrgSuperAdmin','orgsuperadmin'],
+            ['Practitioner','practitioner'],
+        ];
+        foreach ($roles as $role) {
+            Role::create(['name' => $role['0'],'slug'=>$role[1]]);
+        }
+        $users = [
+            ['nissi','nissi@drtele.co','9715555555',bcrypt('nissi@drtele'),1,1,now(),Str::uuid()],
+            ['akhil','akhil@drtele.co','97155555525',bcrypt('akhil@drtele'),1,1,now(),Str::uuid()],
+        ];
+        foreach ($users as $user) {
+            $details = User::create([
+                'username' => $user[0],
+                'email'=>$user[1],
+                'phone_number'=>$user[2],
+                'password'=>$user[3],
+                'profession_id'=>$user[4],
+                'status'=>$user[5],
+                'email_verified_at'=>$user[6],
+                'uuid'=>$user[7]
+            ]);
+            User_Role::create([
+                'user_id' => $details->id,
+                'role_id' => 1
+            ]);
+        }
         $countries_path = './sql_data/tbl_countries.sql';
         $states_path = './sql_data/tbl_states.sql';
         $cities_path = './sql_data/tbl_cities.sql';
